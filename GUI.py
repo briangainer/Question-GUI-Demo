a984f72ac59c2190d1eccc48d5a89bb5ca6029ab
STEM Coding <stemcoding@192.168.1.109>
2018-03-21 22:57:08 -0400

    refactored and added some method bodies

--- GUI.py
+++ GUI.py
@@ -10,45 +10,46 @@ class GUI(Frame):
         self.root.grid_columnconfigure(3, weight=1)
         self.root.geometry("500x500") # set window size
         self.root.title('Research Demo') # add a window title
-        # 1st section
-        self.label_1 = Label(self.root, text='This is the First Question', font='helvetica 20')
-        self.entry_1 = Entry(self.root)
-        self.button_1 = Button(self.root, text="Submit", command=self.press_1)
-        self.label_1.grid(row=1, column=1)
-        self.entry_1.grid(row=2, column=1, pady=(8, 25))
-        self.button_1.grid(row=2, column=2, pady=(8, 25))
-        # 2nd section
-        self.label_2 = Label(self.root, text='This is the Second Question', font='helvetica 20')
-        self.entry_2 = Entry(self.root)
-        self.button_2 = Button(self.root, text="Submit", command=self.press_2)
-        self.label_2.grid(row=3, column=1)
-        self.entry_2.grid(row=4, column=1, pady=(8, 25))
-        self.button_2.grid(row=4, column=2, pady=(8, 25))
-        # 3rd section
-        self.label_3 = Label(self.root, text='This is the Third Question', font='helvetica 20')
-        self.entry_3 = Entry(self.root)
-        self.button_3 = Button(self.root, text="Submit", command=self.press_3)
-        self.label_3.grid(row=5, column=1)
-        self.entry_3.grid(row=6, column=1, pady=(8, 25))
-        self.button_3.grid(row=6, column=2, pady=(8, 25))
-        # 4th section
-        self.label_4 = Label(self.root, text='This is the Fourth Question', font='helvetica 20')
-        self.entry_4 = Entry(self.root)
-        self.button_4 = Button(self.root, text="Submit", command=self.press_4)
-        self.label_4.grid(row=7, column=1)
-        self.entry_4.grid(row=8, column=1, pady=(8, 25))
-        self.button_4.grid(row=8, column=2, pady=(8, 25))
-        self.root.mainloop() # code after this line will execute after window is closed
 
-    # these 4 methods print what is in the text box next to it
-    def press_1(self):
-        print(self.entry_1.get())
+        self.labels = [] # create labels
+        for i in range(4):
+            self.labels.append( Label(self.root, text='Question 1', font='helvetica 20') )
+            self.labels[i].grid(row=(2*i+1), column=1)
 
-    def press_2(self):
-        print(self.entry_2.get())
+        self.entries = [] # create text boxes
+        for j in range(4):
+            self.entries.append( Entry(self.root) )
+            self.entries[j].grid(row=(2*j+2), column=1, pady=(8, 25))
 
-    def press_3(self):
-        print(self.entry_3.get())
+        self.buttons = [] # create buttons
+        for k in range(4):
+            self.buttons.append( Button(self.root, text="Submit", command=(lambda n=k: self.pressed(n))) )
+            self.buttons[k].grid(row=(2*k+2), column=2, pady=(8, 25))
+            
+        self.root.mainloop()
 
-    def press_4(self):
-        print(self.entry_4.get())
+    # this method responds to a button press
+    def pressed(self, num):
+        print(self.entries[num].get())
+
+    ########################
+    ## HIDE A SCREEN SECTION
+    ## num: the number of the section that you want to hide [1 - 4]
+    ## Example to hide the first section -> hideSection(1)
+    def hideSection(self, num):
+        pass
+
+    ########################
+    ## SHOW A SCREEN SECTION
+    ## num: the number of the section that you want to show [1 - 4]
+    ## Example to show the first section -> showSection(1)
+    def showSection(self, num):
+        pass
+
+    ##############################
+    ## CHANGE A SECTION'S QUESTION
+    ## num: the number of the section that has the question [1 - 4]
+    ## text: the updated question
+    ## Example to change the first question -> changeQuestion(1, 'my new question')
+    def changeQuestion(self, num, text):
+        pass
